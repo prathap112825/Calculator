f1785fd5995ef4b5b356afc58adc2ba8713df2fb
prathap112825 <prathapsai112825@gmail.com>
2023-04-28 11:54:34 +0530

    starting

---
+++ src/ex.js
@@ -0,0 +1,36 @@
+/*import React from 'react';
+
+const Button = ({ label, onClick }) => {
+  return (
+    <button onClick={onClick}>
+      {label}
+    </button>
+  );
+};
+
+export default Button;*/
+
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.handleIncrement}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
+
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom/client';
+
+/*import './index.css';
+import App from './App';
+import Parent from './parent';
+import './calendar.css';
+import Calendar from './calendar';*/
+import Counte from './ex';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <div>
+    <Counte/>
+    </div>
+  );
+  
